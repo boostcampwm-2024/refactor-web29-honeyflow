9a6acbccad378c34acae9b9aa70540908fd8ce5d
fru1tworld <127650598+fru1tworld@users.noreply.github.com>
2025-01-22 15:43:10 +0900

    Wesocket서버 및 API 서버 분리  (#3)
    * fix: api 서버와 websocket 분리를 위해 backend 서버 네임 변경
    
    * feature/split api websocket
    
    * fix: 분리 후 중복되는 코드 삭제

---
+++ packages/api/src/app.module.ts
@@ -0,0 +1,41 @@
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { getMongooseConfig } from './common/config/mongo.config';
+import { getTypeOrmConfig } from './common/config/typeorm.config';
+import { NoteModule } from './note/note.module';
+import { SpaceModule } from './space/space.module';
+import { TestModule } from './test/test.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: getMongooseConfig,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: getTypeOrmConfig,
+    }),
+    SpaceModule,
+    NoteModule,
+    TestModule,
+  ],
+})
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  async onModuleInit(): Promise<void> {
+    this.logger.debug('Application initialized for debug');
+    this.logger.log('Application initialized', {
+      module: 'AppModule',
+      environment: process.env.NODE_ENV ?? 'development',
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
---
+++ packages/collaborative/src/space/space.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { SpaceDocument, SpaceSchema } from './space.schema';
+import { SpaceService } from './space.service';
+import { SpaceValidation } from './space.validation.service';
+import { NoteModule } from 'src/note/note.module';
+
+@Module({
+  imports: [
+    NoteModule,
+    MongooseModule.forFeature([
+      { name: SpaceDocument.name, schema: SpaceSchema },
+    ]),
+  ],
+  providers: [SpaceService, SpaceValidation],
+  exports: [SpaceService],
+})
+export class SpaceModule {}
