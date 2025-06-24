f67fcbf635829a13f9c71d0dc23f358ea2825e12
Byeongju Park <beyondzux@gmail.com>
2025-06-24 11:30:47 +0900

    chore: allowed-origins의 IP 주소 변경

--- packages/collaborative/src/main.ts
+++ packages/collaborative/src/main.ts
@@ -16,8 +16,8 @@ const ALLOWED_ORIGINS = [
   'http://localhost:3000',
   'http://localhost:4173',
   'http://localhost:5173',
-  'http://13.239.239.190',
-  'http://13.239.239.190:80'
+  'http://52.64.44.0',
+  'http://52.64.44.0:80'
 ] as string[];
 
 function configureGlobalSettings(app: INestApplication) {
